2139133c753c2d07cbdd62403c603d0e6ec4cc3e
shmiak <yakubov.sha@gmail.com>
2024-03-21 11:23:43 +0100

    Add clasifiers

--- blooms_ml/utils.py
+++ blooms_ml/utils.py
@@ -1,3 +1,5 @@
+import itertools
+
 import numpy as np
 import pandas as pd
 import xarray as xr
@@ -70,3 +72,39 @@ def plot_variable(variable: pd.DataFrame):
     fig, ax = plt.subplots(figsize=(15, 5))
     cf = ax.contourf(X, Y, variable.values)
     fig.colorbar(cf, ax=ax, location='right', pad=0.01)
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    from: www.kaggle.com .. credit-fraud-dealing-with-imbalanced-datasets
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title, fontsize=14)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
